21b9ce3264d68ecefe8113721ba5a8da41b905d6
JianqinWang <jianqin.wang@scality.com>
2018-01-15 17:16:51 -0800

    ft: add tests for compactor

--- src/test/scala/com/scality/clueso/CompactionSpec.scala
+++ src/test/scala/com/scality/clueso/CompactionSpec.scala
@@ -7,30 +7,82 @@ import org.scalatest.{Matchers, WordSpec}
 
 class CompactionSpec extends WordSpec with Matchers with SparkContextSetup {
   "Table Compactor" should {
-    "Scenario 1: compact staging files" in withSparkContext {
+    "Scenario 1: compact staging files with force" in withSparkContext {
       (spark, config) =>
         val bucketName = "compactmebucket"
         val numberParquetFiles = "10"
         val configPath = getClass.getResource("/application.conf").toString.substring(5)
+        val fs = SparkUtils.buildHadoopFs(config)
+        val compactor = new TableFilesCompactor(spark, config)
 
         // populate
         LandingMetadataPopulatorTool.main(Array(configPath, bucketName, "100", numberParquetFiles))
-        val fs = SparkUtils.buildHadoopFs(config)
+
+        // check landing files are created
         fs.listStatus(new Path(config.landingPathUri, s"bucket=$bucketName/maxOpIndex=$numberParquetFiles"),
           SparkUtils.parquetFilesFilter).length shouldEqual numberParquetFiles.toInt
 
         // compact
-        val compactor = new TableFilesCompactor(spark, config)
         compactor.compactLandingPartition("bucket", bucketName, 1, true)
 
         // then, landing should be empty
-        fs.listStatus(new Path(PathUtils.landingURI, s"bucket=$bucketName"), SparkUtils.parquetFilesFilter).length shouldEqual 0
+        fs.listStatus(new Path(config.landingPathUri, s"bucket=$bucketName"), SparkUtils.parquetFilesFilter).length shouldEqual 0
 
-        // then staging should have...
+        // then staging should have new object
+        fs.listStatus(new Path(config.stagingPathUri, s"bucket=$bucketName/maxOpIndex=$numberParquetFiles"),
+          SparkUtils.parquetFilesFilter).length shouldEqual 1
+    }
 
+    "Scenario 2: compact staging files without specifying bucket name" in withSparkContext {
+      (spark, config) =>
+        val bucketName = "compactnoname"
+        val numberParquetFiles = "10"
+        val configPath = getClass.getResource("/application.conf").toString.substring(5)
+        val fs = SparkUtils.buildHadoopFs(config)
+        val compactor = new TableFilesCompactor(spark, config)
 
-        // do tests without force true and without specifying a bucket name
-        
+        // populate
+        LandingMetadataPopulatorTool.main(Array(configPath, bucketName, "100", numberParquetFiles))
+
+        // check landing files were created correctly
+        fs.listStatus(new Path(config.landingPathUri, s"bucket=$bucketName/maxOpIndex=$numberParquetFiles"),
+          SparkUtils.parquetFilesFilter).length shouldEqual numberParquetFiles.toInt
+
+        //compact
+        compactor.compactLandingPartition("bucket", "", 1, true)
+
+        // landing should be empty
+        fs.listStatus(new Path(config.landingPathUri, s"bucket=$bucketName"), SparkUtils.parquetFilesFilter).length shouldEqual 0
+
+        // should show staging obejct
+        fs.listStatus(new Path(config.stagingPathUri, s"bucket=$bucketName/maxOpIndex=$numberParquetFiles"),
+          SparkUtils.parquetFilesFilter).length shouldEqual 1
+    }
+
+    "Scenario 3: compact staging files without having to force compaction" in withSparkContext {
+      (spark, config) =>
+        val bucketName = "compactnoforce"
+        val numberParquetFiles = "10"
+        val configPath = getClass.getResource("/application.conf").toString.substring(5)
+        val fs = SparkUtils.buildHadoopFs(config)
+        val compactor = new TableFilesCompactor(spark, config)
+
+        // populate
+        LandingMetadataPopulatorTool.main(Array(configPath, bucketName, "1000", numberParquetFiles))
+
+        // check that landing files were created
+        fs.listStatus(new Path(config.landingPathUri, s"bucket=$bucketName/maxOpIndex=$numberParquetFiles"),
+          SparkUtils.parquetFilesFilter).length shouldEqual numberParquetFiles.toInt
+
+        // compact
+        compactor.compactLandingPartition("bucket", bucketName, 1, false)
+
+        // check landing (should be empty again)
+        fs.listStatus(new Path(config.landingPathUri, s"bucket=$bucketName"), SparkUtils.parquetFilesFilter).length shouldEqual 0
+
+        // should now have staging object
+        fs.listStatus(new Path(config.stagingPathUri, s"bucket=$bucketName/maxOpIndex=$numberParquetFiles"),
+          SparkUtils.parquetFilesFilter).length shouldEqual 1
     }
   }
 }
